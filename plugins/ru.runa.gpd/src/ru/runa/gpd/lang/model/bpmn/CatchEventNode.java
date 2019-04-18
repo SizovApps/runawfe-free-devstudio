f5dd7071acb0f509c2217286b98d1881c6c04849
Dofs <dofs197@gmail.com>
2019-04-18 07:17:39 +0300

    Revert "fix bug"
    This reverts commit 4817b8edf21fdc2bccd33a1e8b4334bf13135f74.

--- plugins/ru.runa.gpd/src/ru/runa/gpd/lang/model/bpmn/CatchEventNode.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/lang/model/bpmn/CatchEventNode.java
@@ -1,7 +1,7 @@
 package ru.runa.gpd.lang.model.bpmn;
 
 import java.util.List;
-import org.eclipse.core.resources.IFile;
+
 import ru.runa.gpd.lang.ValidationError;
 import ru.runa.gpd.lang.model.IReceiveMessageNode;
 import ru.runa.gpd.lang.model.Timer;
@@ -19,17 +19,5 @@ public class CatchEventNode extends AbstractEventNode implements IReceiveMessage
             return;
         }
         super.validateOnEmptyRules(errors);
-    }
-
-    @Override
-    public void validate(List<ValidationError> errors, IFile definitionFile) {
-        List<CatchEventNode> catchEventNodes = getChildren(CatchEventNode.class);
-        for (CatchEventNode catchEventNode : catchEventNodes) {
-            if (catchEventNode.getEventNodeType() != EventNodeType.error) {
-                errors.add(ValidationError.createLocalizedError(this, "catchEventNode.notErrorType"));
-            }
-        }
-
-        super.validate(errors, definitionFile);
     }
 }
