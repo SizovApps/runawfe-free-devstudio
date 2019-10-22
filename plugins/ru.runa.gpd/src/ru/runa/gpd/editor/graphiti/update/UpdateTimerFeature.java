5c39096daba9c7cf24cc88e66ab5df6d80fc718d
dataobject <dataobject@yandex.ru>
2019-10-22 13:30:57 +0300

    RM1320

--- plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/update/UpdateTimerFeature.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/update/UpdateTimerFeature.java
@@ -1,13 +1,12 @@
 package ru.runa.gpd.editor.graphiti.update;
 
+import com.google.common.base.Objects;
 import org.eclipse.graphiti.features.IReason;
 import org.eclipse.graphiti.features.context.IUpdateContext;
 import org.eclipse.graphiti.features.impl.Reason;
-import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
+import org.eclipse.graphiti.mm.algorithms.Image;
 import org.eclipse.graphiti.mm.pictograms.ContainerShape;
-
-import ru.runa.gpd.editor.graphiti.GaProperty;
-import ru.runa.gpd.editor.graphiti.PropertyUtil;
+import org.eclipse.graphiti.services.Graphiti;
 import ru.runa.gpd.lang.model.ITimed;
 import ru.runa.gpd.lang.model.Timer;
 
@@ -17,8 +16,7 @@ public class UpdateTimerFeature extends UpdateFeature {
     public IReason updateNeeded(IUpdateContext context) {
         Timer timer = (Timer) getBusinessObjectForPictogramElement(context.getPictogramElement());
         if (timer.getParent() instanceof ITimed) {
-            GraphicsAlgorithm boundaryEllipse = PropertyUtil.findGaRecursiveByName(context.getPictogramElement(), GaProperty.BOUNDARY_ELLIPSE);
-            if (timer.isInterruptingBoundaryEvent() == boundaryEllipse.getLineVisible()) {
+            if (!Objects.equal(((Image) ((ContainerShape) context.getPictogramElement()).getGraphicsAlgorithm()).getId(), getImageId(timer))) {
                 return Reason.createTrueReason();
             }
         }
@@ -29,10 +27,16 @@ public class UpdateTimerFeature extends UpdateFeature {
     public boolean update(IUpdateContext context) {
         ContainerShape containerShape = (ContainerShape) context.getPictogramElement();
         Timer timer = (Timer) getBusinessObjectForPictogramElement(containerShape);
-        GraphicsAlgorithm boundaryEllipse = PropertyUtil.findGaRecursiveByName(containerShape, GaProperty.BOUNDARY_ELLIPSE);
-        boundaryEllipse.setLineVisible(!timer.isInterruptingBoundaryEvent());
-        layoutPictogramElement(containerShape);
+        String imageId = getImageId(timer);
+        if (!Objects.equal(((Image) containerShape.getGraphicsAlgorithm()).getId(), imageId)) {
+            Image oldImage = (Image) containerShape.getGraphicsAlgorithm();
+            Image newImage = Graphiti.getGaService().createImage(containerShape, imageId);
+            Graphiti.getGaService().setLocationAndSize(newImage, oldImage.getX(), oldImage.getY(), oldImage.getWidth(), oldImage.getHeight());
+        }
         return true;
     }
 
+    private String getImageId(Timer timer) {
+        return "graph/boundary_timer" + (!timer.isInterruptingBoundaryEvent() ? "_non_interrupting" : "") + ".png";
+    }
 }
